8956c1bd21020038fa018217059750705e2e823c
krumetoft <krum.arnaudov@ft.com>
2023-02-06 11:36:08 +0200

    first working api

--- app.py
+++ app.py
@@ -1,45 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from pydantic import BaseModel, validator
 import time
 import asyncio
 
 from ml import extract_keywords 
 
+# class Text(BaseModel):
+#     text: str
+    
+
+# class Textlist(BaseModel):
+#     articles: list(Text)
+    
+#     @validator("articles", pre=True, each_item=True)
+#     def each_item_should_be_a_string(cls, articles):
+#         if not all(isinstance(article, str) for article in articles):
+#             raise ValueError(f"All articles should be of type string")
+#         return articles
+
 app = FastAPI()
+
+class TextRequest(BaseModel):
+    text: str
+
+@app.post("/keywords")
+async def generate_keywords(request: Request, text: TextRequest):
+    keywords = extract_keywords(text.text)
+    return{"keywords": keywords}
 
 @app.get("/")
 def root():
     return {"message": "hello world again"}
-
-
-@app.get("/users/{user_id}")
-def read_user(user_id: int):
-    """
-    We accept a 'user_id' and return a json blob.
-    """
-    return {"user_id": user_id}
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    
-    @validator("price")
-    def price_must_be_positive(cls, value):
-        if value <= 0:
-            raise ValueError(f"Price >= 0, we got {value}")
-        return value
-
-@app.post("/items/")
-def create_item(item: Item):
-    return item
-
-@app.get("/sleep_slow")
-def sleep_slow():
-    r = time.sleep(1)
-    return {"status": "done"}
-
-@app.get("/sleep_fast")
-async def sleep_fast():
-    r = await asyncio.sleep(1)
-    return {'status': "done"}
--- ml.py
+++ ml.py
@@ -2,7 +2,7 @@ from keybert import KeyBERT
 from sentence_transformers import SentenceTransformer
 
 
-def extract_keywords(text: list):
+def extract_keywords(text: str):
     """a simple keybert based keywords extraction
 
     Args:
@@ -14,7 +14,7 @@ def extract_keywords(text: list):
     kw_model = KeyBERT(model=sentence_model)
     
     keywords = kw_model.extract_keywords(text, 
-                                         keyphrase_ngram_range=(2, 3), 
+                                         keyphrase_ngram_range=(2, 5), 
                                          stop_words='english',
                                          use_maxsum=True, 
                                          nr_candidates=20, 
